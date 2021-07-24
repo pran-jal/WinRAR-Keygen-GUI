72f935a86282ad5cecd10267df01d17dff617fa5
Pranjal <72604669+pran-jal@users.noreply.github.com>
2021-07-24 10:16:22 -0600

    Update KeygenGUI.h

--- KeygenGUI.h
+++ KeygenGUI.h
@@ -1,3 +1,8 @@
+/*********************************************************************
+* Project:   WinRAR-Keygen-GUI
+* Author:     Pranjal (https://github.com/pran-jal)
+*********************************************************************/
+
 #pragma once
 
 #include <msclr/marshal.h>
